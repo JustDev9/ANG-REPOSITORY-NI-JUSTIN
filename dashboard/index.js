506afddb35242ba2eaab0ba9b1372e5a4abab016
JustFingArround <justindelrosario89@gmail.com>
2024-12-04 17:34:30 +0800

    Update index.js

--- dashboard/index.js
+++ dashboard/index.js
@@ -2,14 +2,10 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 
-
 menuBtn.addEventListener("click", () => {
-    sideMenu.style.display = 'block';
-})
+    sideMenu.classList.add("show");
+});
 
 closeBtn.addEventListener("click", () => {
-    sideMenu.style.display = 'none';
-})
-
-
-;
+    sideMenu.classList.remove("show");
+});
